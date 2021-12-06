0fb1be9ff8bb59a96d0da76b19274d75d6c0f397
zakzakst <59270506+zakzakst@users.noreply.github.com>
2021-12-06 21:06:18 +0900

    ステップ6

---
+++ src/components/atom/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="button">{{ text }}</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    //
+  },
+});
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,11 @@ const routes: Array<RouteRecordRaw> = [
     component: Home,
   },
   {
+    path: '/test',
+    name: 'Test',
+    component: () => import(/* webpackChunkName: 'test' */ '../views/Test.vue'),
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
