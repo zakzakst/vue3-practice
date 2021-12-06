930e79cf1aa184064039e54e17d2347b5004ad4f
zakzakst <59270506+zakzakst@users.noreply.github.com>
2021-12-07 06:19:12 +0900

    ステップ6

--- src/components/atom/Button.vue
+++ src/components/atom/Button.vue
@@ -1,9 +1,9 @@
 <template>
-  <button class="button">{{ text }}</button>
+  <button class="button" :class="classes">{{ text }}</button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, computed, toRefs, ref } from 'vue';
 
 export default defineComponent({
   props: {
@@ -11,9 +11,34 @@ export default defineComponent({
       type: String,
       required: true,
     },
+    color: {
+      // TODO: 入力制限の方法調べる
+      type: String,
+      required: false,
+    },
+    light: {
+      type: Boolean,
+      required: false,
+    },
   },
-  setup() {
-    //
+  setup(props) {
+    const state = reactive({
+      colorClass: computed(() => {
+        if (!props.color) return '';
+        return `is-${props.color}`;
+      }),
+      lightClass: computed(() => {
+        if (!props.light) return '';
+        return `is-light`;
+      }),
+    });
+
+    const classes = ref([state.colorClass, state.lightClass]);
+
+    return {
+      ...toRefs(state),
+      classes,
+    };
   },
 });
 </script>
